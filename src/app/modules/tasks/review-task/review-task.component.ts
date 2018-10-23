4a78fdbfc739db77d995d948bc322b0828657ffa
Jayakrishnan S <jayakrishnans@qburst.com>
2018-10-23 19:04:35 +0530

    Review tasks

--- src/app/modules/tasks/review-task/review-task.component.ts
+++ src/app/modules/tasks/review-task/review-task.component.ts
@@ -14,11 +14,15 @@ export class ReviewTaskComponent implements OnInit {
     loading = false;
     currentUser: any;
     tasksForReview: StudTask[];
+    selectedTask: StudTask;
+    selTaskArray: object[];
+    totalMark: number = 0;
     constructor(
         private apiService: APIService,
         private alertService: AlertService,
         private modalService: ModalService) { }
     ngOnInit() {
+        this.selectedTask = new StudTask();
         this.apiService.getAPICall(`${config.apiUrl}/task/getSubmittedTasks`, '')
             .pipe(first())
             .subscribe(
@@ -36,9 +40,46 @@ export class ReviewTaskComponent implements OnInit {
                     this.loading = false;
                 });
     }
-    reviewTask(task: StudTask) {
-        debugger
+    selectTask(task: StudTask) {
+        this.selectedTask = task;
+        this.selTaskArray = [];
+        for (let thisTask of this.selectedTask.task) {
+            this.selTaskArray.push(thisTask);
+        }
         this.modalService.open('review-modal');
+    }
+    reviewTask() {
+        this.apiService.postAPICall(`${config.apiUrl}/task/listTasks`, { classTitle: this.selectedTask.classTitle, userType: 'teacher' })
+            .pipe(first())
+            .subscribe(
+                data => {
+                    if (data.status === 1) {
+                        this.alertService.success(data.message, true);
+                        this.loading = false;
+                        this.totalMark = 0;
+                        for (let dataresult of data.result) {
+                            for (let selTask of this.selTaskArray) {
+                                if (selTask['question'] === dataresult.question) {
+                                    selTask['correctAns'] = dataresult.solution;
+                                    if (selTask['solution'] === dataresult.solution) {
+                                        selTask['mark'] = 1;
+                                        this.totalMark++;
+                                    }
+                                    else
+                                        selTask['mark'] = 0;
+                                }
+                            }
+                        }
+                        debugger
+                    } else {
+                        this.alertService.error(data.message);
+                        this.loading = false;
+                    }
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
     }
     closeModal(id: string) {
         this.modalService.close(id);
